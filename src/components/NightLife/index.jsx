2817265bc1bcb080bf404ae98e55104ebbf04703
MOHAMED NUMAN NASEER <abcandmum@gmail.com>
2022-01-31 02:00:22 +0530

    modified colors

--- src/components/NightLife/index.jsx
+++ src/components/NightLife/index.jsx
@@ -9,7 +9,7 @@ function Dining() {
       <h1 className="text-xl my-4 md:my-8 md:text-3xl md:font-semibold">
         Collections
       </h1>
-      <h2 className="text-l my-4 md:my-8 md:text-2xl">Explore curated lists of top pubs, bars and clubs in Bengaluru, based on trends. <span style={{"font-size":"12px","color":"red"}}>Covid-19 guidelines followed</span> </h2> 
+      <h2 className="text-l my-4 md:my-8 md:text-2xl">Explore curated lists of top pubs, bars and clubs in Bengaluru, based on trends. <span style={{"font-size":"12px","color":"brown"}}>Covid-19 guidelines followed</span> </h2> 
       <NightLifeCarousel />
     </div>
   );
